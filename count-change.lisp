e945e1bd9113edaa65ecf4c4dd9e033425cab424
buyi <huaqirong@126.com>
2016-03-10 23:16:14 +0800

    fix count-change error

--- count-change.lisp
+++ count-change.lisp
@@ -8,9 +8,10 @@
 			            (cc (- amount (first-denmination kinds-of-coins))
 			            kinds-of-coins)))))
 
-(defin (first-denomination kinds-of-coins)
+(define (first-denmination kinds-of-coins)
 		(cond ((= kinds-of-coins 1) 1)
 				((= kinds-of-coins 2) 5)
 				((= kinds-of-coins 3) 10)
-				((= kinds-of-coins 4) 10)
+				((= kinds-of-coins 4) 25)
 				((= kinds-of-coins 5) 50)))
+(count-change 100)
